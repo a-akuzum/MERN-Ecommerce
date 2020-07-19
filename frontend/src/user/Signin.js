471494a987f31c00a1ea71b45dec04b27442bafe
Aaron <a.akuzum@gmail.com>
2020-07-18 21:16:31 -0700

    frontend

--- frontend/src/user/Signin.js
+++ frontend/src/user/Signin.js
@@ -3,3 +3,15 @@ import React from 'react'
 const Signin = () => <div>signin</div>
 
 export default Signin;
+
+
+import React from 'react'
+import Layout from './Layout'
+
+const Home = () => (
+    <Layout title="Home Page" description="E-Commerce App">
+        ...
+    </Layout>
+)
+
+export default Home;
