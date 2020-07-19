eae64c08e305fd118fd4b4d4aba7162a52073121
Aaron <a.akuzum@gmail.com>
2020-07-18 21:14:06 -0700

    frontend

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import Layout from './Layout'
 
-
-const Home = () => <div>Home</div>
+const Home = () => (
+    <Layout title="Home Page" description="E-Commerce App">
+        ...
+    </Layout>
+)
 
 export default Home;
