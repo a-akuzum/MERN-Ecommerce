a87aad45c8187f867729a172fe7e537d0f38a8d2
Aaron <a.akuzum@gmail.com>
2020-07-19 20:58:04 -0700

    frontend

--- frontend/src/core/Menu.js
+++ frontend/src/core/Menu.js
@@ -13,15 +13,15 @@ const Menu = () => (
     <div>
         <ul className="nav nav-tabs bg-primary">
             <li className="nav-item">
-                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" style={isActive(history, '/')} to="/">Home</Link>
             </li> 
 
             <li className="nav-item">
-                <Link className="nav-link" to="/signin">Signin</Link>
+                <Link className="nav-link" style={isActive(history, '/signin')} to="/signin">Signin</Link>
             </li> 
 
             <li className="nav-item">
-                <Link className="nav-link" to="/signup">Signup</Link>
+                <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">Signup</Link>
             </li> 
         </ul>
     </div>
