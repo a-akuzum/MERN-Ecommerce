a17788ef96e6ce3ee0051762c030d8e42346c157
Aaron <a.akuzum@gmail.com>
2020-07-19 21:02:21 -0700

    frontend

--- frontend/src/core/Menu.js
+++ frontend/src/core/Menu.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link, withRouter} from 'react-router-dom'
 
 const isActive = (history, path) => {
-    if (history.location.path === path){
+    if (history.location.pathname === path){
         return {color: '#ff9900'};
     }else{
         return {color: "#ffffff"};
