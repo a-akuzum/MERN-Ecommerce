4a28855a6b078719a8cead119b889d496d0a246c
Aaron <a.akuzum@gmail.com>
2020-07-20 20:51:34 -0700

    frontend

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,11 +3,13 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom' // to access props
 import Signup from './user/Signup'
 import Signin from './user/Signin'
 import Home from './core/Home'
+import Menu from './core/Menu'
 
 //instead of div, BrowserRouter used
 const Routes = () => {
     return (
         <BrowserRouter> 
+            <Menu />
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/signin" exact component={Signin} />
