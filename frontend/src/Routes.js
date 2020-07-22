81f27f36a7b75344d0eefa7b680041cc2ad989ce
Aaron <a.akuzum@gmail.com>
2020-07-21 17:16:58 -0700

    frontend

---
+++ frontend/src/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom' // to access props on browser
+import Signup from './user/Signup'
+import Signin from './user/Signin'
+
+const Routes = () => {
+    return (
+        <div>
+            
+        </div> 
+    )
+}
