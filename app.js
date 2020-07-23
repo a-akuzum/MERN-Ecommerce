66b30dbc793dac2cceae5aa7dd595f8ea678f164
Aaron <a.akuzum@gmail.com>
2020-07-22 21:52:59 -0700

    adds

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const cors = require('cors') //this is for if clientside using differnt port or api
 const expressValidator = require('express-validator')
 const dotenv = require('dotenv');
 dotenv.config()
+
 //import routes
 const authRoutes = require('./routes/auth')
 const userRoutes = require('./routes/user')
@@ -21,6 +23,7 @@ app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(expressValidator())
+app.use(cors())
 
 //routes middleware
 app.use('/api',authRoutes)
