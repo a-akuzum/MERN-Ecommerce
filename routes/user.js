cb2d0ddab7e4d2719e2df70caf68849d449830c6
Aaron <a.akuzum@gmail.com>
2020-07-23 21:38:58 -0700

    adds

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { userById } = require("../controllers/user")
+const { userById, read, update } = require("../controllers/user")
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth')
 
 router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
