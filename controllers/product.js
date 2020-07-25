66912417fb855e658731af402e9d7fff0239dd1b
Aaron <a.akuzum@gmail.com>
2020-07-24 21:35:24 -0700

    adds

--- controllers/product.js
+++ controllers/product.js
@@ -240,9 +240,9 @@ exports.listBySearch = (req, res) => {
 }
 
 exports.photo = (req, res, next) => {
-    if(req.product.photo.date){
+    if(req.product.photo.data){
         res.set('Content-Type', req.product.photo.contentType)
-        return res.send(req.product.photo.date)
+        return res.send(req.product.photo.data)
     }
 
     next();
