93aaa3513fddbde9dc8e5c37305be339cf7137ac
Aaron <a.akuzum@gmail.com>
2020-07-31 15:22:43 -0700

    adds

--- controllers/category.js
+++ controllers/category.js
@@ -63,6 +63,11 @@ exports.remove = (req, res) => {
 
 exports.list = (req, res) => {
     Category.find().exec((err, data) => {
-        
+        if(err){
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+        res.json(data)
     })
 }
