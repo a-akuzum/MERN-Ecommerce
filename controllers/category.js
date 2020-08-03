884e1027e4c6277746d864832e982fea4b78124d
Aaron <a.akuzum@gmail.com>
2020-08-03 14:18:23 -0700

    adds

--- controllers/category.js
+++ controllers/category.js
@@ -1,7 +1,18 @@
 const Category = require('../models/category')
 const {errorHandler} = require('../helpers/db-ErrorHandler')
 
+exports.categoryById = (res, req, next, id) => {
+    Category.findById(id).exec((err, category) => {
+        if (err || !category){
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
 
+        req.category = category
+        next();
+    })
+}
 
 exports.create = (req, res) => {
     const category = new Category(req.body)
