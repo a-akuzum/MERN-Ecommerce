829cf26f923520ea86c36a72ea268bec680ff98d
Aaron <a.akuzum@gmail.com>
2020-08-03 14:21:12 -0700

    adds

--- routes/category.js
+++ routes/category.js
@@ -5,13 +5,13 @@ const { userById } = require("../controllers/user")
 const { create, categoryById } = require("../controllers/category")
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth')
 
-
+router.get('/category/:categoryId', read)
 router.post(
     "/category/create/:userId",
     requireSignin,
     isAuth,
     isAdmin, 
-    create)
+    create) // create method on category.js on controller
 
 router.param('categoryId', categoryById)
 router.param('userId', userById)
