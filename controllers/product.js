cbab26999d8e12c4f5b1e756ff479bf64eed0cd7
Aaron <a.akuzum@gmail.com>
2020-08-04 13:28:39 -0700

    adds

--- controllers/product.js
+++ controllers/product.js
@@ -82,3 +82,48 @@ exports.remove = (req, res) => {
 
     })
 }
+
+exports.create = (req, res) => {
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true
+    form.parse(req, (err, fields, files) => {
+        if(err){
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            })
+        }
+        let product = new Product(fields)
+
+        // check for all fields
+        const {name, description, price, category, quantity, shipping} = fields
+        if(!name || !description || !price || !category || !quantity || !shipping){
+            return res.status(400).json({
+                error: 'All fields are required'
+            })
+        }
+        
+        //photo validation
+        // 1kb = 1000
+        // 1mb = 1000000
+        if(files.photo){
+            // console.log("FILES PHOTO: ", files.photo)
+            if(files.photo.size > 1000000){
+                return res.status(400).json({
+                    error: 'Image size should be less than 1Mb'
+                })
+            }
+            
+            product.photo.data = fs.readFileSync(files.photo.path) //fs = filesystem
+            product.photo.contentType = files.photo.type
+        }
+
+        product.save((err, result) => {
+            if(err){
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(result)
+        })
+    })
+}
