59c79271b1e05f0aba113e432a5691c73dcd7d70
Aaron <a.akuzum@gmail.com>
2020-08-09 17:36:52 -0700

    adds

--- controllers/product.js
+++ controllers/product.js
@@ -8,7 +8,9 @@ const {errorHandler} = require('../helpers/db-ErrorHandler')
 exports.productById = (req, res, next, id) => {
     Product.findById(id).exec((err, product) => {
     if(err || !product){
-        
+        return res.status(400).json({
+            error: 'Product not found'
+        })
     }
     })
 }
