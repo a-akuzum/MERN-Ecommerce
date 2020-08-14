ab350135254b7561aba22b6c3d80e100ef0a586a
Aaron <a.akuzum@gmail.com>
2020-08-14 16:14:50 -0700

    adds

--- controllers/product.js
+++ controllers/product.js
@@ -4,7 +4,10 @@ const _ = require('lodash')
 const Product = require('../models/product')
 
 
-
 exports.create = (req, res) => {
-
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true
+    form.parse(req, () => {
+        
+    })
 }
