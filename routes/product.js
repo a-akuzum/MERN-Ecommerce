0fedee9b4c9d4170c8587389bb8672ed932dd69f
Aaron <a.akuzum@gmail.com>
2020-08-15 16:07:39 -0700

    adds

---
+++ routes/product.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+const { userById } = require("../controllers/user")
+
+const { create } = require("../controllers/category")
+const { requireSignin, isAdmin, isAuth } = require('../controllers/auth')
+
+
+router.post(
+    "/category/create/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin, 
+    create)
+
+router.param('userId', userById)
+
+
+module.exports = router;
