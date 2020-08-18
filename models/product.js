4f1c3e03222873f50006125a7a5042d16bb55bdd
Aaron <a.akuzum@gmail.com>
2020-08-18 15:55:05 -0700

    adds

--- models/product.js
+++ models/product.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
-const crypto = require('crypto')
-// const { v1: uuidv1 } = require('uuid');
-const uuidv1 = require('uuidv1')
+
 
 const userSchema = new mongoose.Schema({
     name: {
