4f641e2989e1e5ada649399c032f57edaa252012
Aaron <a.akuzum@gmail.com>
2020-08-19 15:44:48 -0700

    adds

--- routes/category.js
+++ routes/category.js
@@ -5,7 +5,7 @@ const { create } = require("../controllers/category")
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth')
 
 
-router.post("/category/create",requireSignin,isAuth,isAdmin, create)
+router.post("/category/create/:userId",requireSignin,isAuth,isAdmin, create)
 
 
 
