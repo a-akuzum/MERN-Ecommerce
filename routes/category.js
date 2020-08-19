331599c60781be85cc23977206f3ff5cb078deb3
Aaron <a.akuzum@gmail.com>
2020-08-19 15:30:40 -0700

    adds

--- routes/category.js
+++ routes/category.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const {
-    create
-} = require("../controllers/category")
+const { create } = require("../controllers/category")
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth')
 
 
