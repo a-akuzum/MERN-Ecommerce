dbb474f27fb4f476096463974bc6d00bda161703
Aaron <a.akuzum@gmail.com>
2020-08-21 15:16:54 -0700

    adds

--- models/category.js
+++ models/category.js
@@ -9,29 +9,6 @@ const userSchema = new mongoose.Schema({
         trim: true,
         required: true,
         maxlength: 32
-    },
-    email: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: 32
-    },
-    hashed_password: {
-        type: String,
-        required: true,
-    },
-    about: {
-        type: String,
-        trim: true,
-    },
-    salt: String,
-    role: {
-        type: Number,
-        default: 0
-    },
-    history: {
-        type: Array,
-        default: []
     }
 },
  { timestamps: true}
